e7c7ba166c2d422a68aaa6a872ce9795bf5f2e47
nopynospy <nopynospy@gmail.com>
2021-10-07 11:48:39 +0800

    Update update script. Add imputation and preprocessing notebooks

--- merge.py
+++ merge.py
@@ -1,13 +1,14 @@
 import json
 
-old = json.load(open("fmt_headlines_from_jan2020.json", encoding="utf8"))
-current = json.load(open("oct_1.json", encoding="utf8"))
+old = json.load(open("jan2020_to_5-10-21.json", encoding="utf8"))
+current = json.load(open("oct_6.json", encoding="utf8"))[0]['items']
 
 def DictListUpdate( lis1, lis2):
+    lis2 = lis2
     for aLis1 in lis1:
         if aLis1 not in lis2:
             lis2.append(aLis1)
     return lis2
 
-with open('merged.json', 'w') as f:
+with open('jan2020_to_6-10-21.json', 'w') as f:
     json.dump(DictListUpdate(old, current), f)
