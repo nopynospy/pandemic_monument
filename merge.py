4a4aefaf0801803044efdd25fe866e3e53f49e83
nopynospy <nopynospy@gmail.com>
2021-10-29 16:22:10 +0800

    Fix october bug

--- merge.py
+++ merge.py
@@ -10,12 +10,16 @@ for c in json.load(open("bug_fix.json", encoding="utf8")):
     current.append(c)
 
 def DictListUpdate( lis1, lis2):
-    lis2 = lis2[0]['items']
-
+    lis2 = lis2
+    new_list = []
+    for ls in lis2:
+        new_list.append(ls['items'])
+    new_list = list(reversed(new_list))
+    new_list = [item for sublist in new_list for item in sublist]
     for aLis1 in lis1:
-        if aLis1 not in lis2:
-            lis2.append(aLis1)
-    return lis2
+        if aLis1 not in new_list:
+            new_list.append(aLis1)
+    return new_list
 
-with open('jan2020_to_28-10-21.json', 'w') as f:
+with open('jan2020_to_28-10-21b.json', 'w') as f:
     json.dump(DictListUpdate(old, current), f)
