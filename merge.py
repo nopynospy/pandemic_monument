90f213c9c03fb0117e7b1585530329ec270296c2
nopynospy <nopynospy@gmail.com>
2021-10-29 15:24:41 +0800

    Fix merge bug. Update 28/10/2021

--- merge.py
+++ merge.py
@@ -3,15 +3,19 @@
 
 import json
 
-old = json.load(open("jan2020_to_17-10-21.json", encoding="utf8"))
-current = json.load(open("oct_18.json", encoding="utf8"))[0]['items']
+old = json.load(open("jan2020_to_27-10-21.json", encoding="utf8"))
+# current = json.load(open("oct_27.json", encoding="utf8"))[0]['items']
+current = []
+for c in json.load(open("oct_28.json", encoding="utf8")):
+    current.append(c)
 
 def DictListUpdate( lis1, lis2):
-    lis2 = lis2
+    lis2 = lis2[0]['items']
+
     for aLis1 in lis1:
         if aLis1 not in lis2:
             lis2.append(aLis1)
     return lis2
 
-with open('jan2020_to_18-10-21.json', 'w') as f:
+with open('jan2020_to_28-10-21.json', 'w') as f:
     json.dump(DictListUpdate(old, current), f)
