a1359ab1780a15dd1c38b6b776d629b00aae246b
nopynospy <nopynospy@gmail.com>
2021-12-09 09:01:05 +0800

    Update 8/12/21

--- merge.py
+++ merge.py
@@ -3,10 +3,10 @@
 
 import json
 
-old = json.load(open("jan2020_to_3-11-21.json", encoding="utf8"))
+old = json.load(open("jan2020_to_7-11-21.json", encoding="utf8"))
 # current = json.load(open("oct_27.json", encoding="utf8"))[0]['items']
 current = []
-for c in json.load(open("nov_7.json", encoding="utf8")):
+for c in json.load(open("dec_8.json", encoding="utf8")):
     current.append(c)
 
 def DictListUpdate( lis1, lis2):
@@ -21,5 +21,5 @@ def DictListUpdate( lis1, lis2):
             new_list.append(aLis1)
     return new_list
 
-with open('jan2020_to_7-11-21.json', 'w') as f:
+with open('jan2020_to_8-12-21.json', 'w') as f:
     json.dump(DictListUpdate(old, current), f)
